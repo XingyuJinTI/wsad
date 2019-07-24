88d3bc9b3e07e8a9129a648ddff064bb24e9501b
Eddie Jin <jinxingyu95@gmail.com>
2019-07-24 16:08:32 +0100

    upsample feature

--- wsgn.py
+++ wsgn.py
@@ -65,7 +65,50 @@ class Loc_Sigmoid(nn.Module):
         means = torch.mean(x, self.dim, keepdim=True)
         x_exp = torch.exp(self.alpha * (self.beta * means - x)) # because multiplied with -1
         return 1 / (1 + x_exp)
-    
+
+
+class Glob_Sigmoid(nn.Module):
+    def __init__(self, dim, alpha, beta, num_classes):
+        super(Loc_Sigmoid, self).__init__()
+        self.dim = dim
+        self.alpha = alpha
+        self.beta = beta
+        self.mean = nn.Parameter(torch.randn(num_classes))
+        
+    def forward(self, x):
+        x_exp = torch.exp(self.alpha * (self.beta * self.mean.expand(x.size()[0:2]).unsqueeze(2).expand_as(x) - x)) # because multiplied with -1
+        return 1 / (1 + x_exp)
+
+
+class WSSN(nn.Module):
+    def __init__(self, num_classes=400, spatial_squeeze=True, name='wsgn', dropout_keep_prob=0.8, mode='rgb'):
+        super(WSSN, self).__init__()
+        self._num_classes = num_classes
+        self._spatial_squeeze = spatial_squeeze
+
+        self.dropout = nn.Dropout(dropout_keep_prob)
+        self.cls_logits = nn.Conv2d(in_channels=1024, out_channels=self._num_classes, kernel_size=1)
+        self.loc_logits = nn.Conv2d(in_channels=1024, out_channels=self._num_classes, kernel_size=1)
+        self.s_Cls = nn.Softmax(1)
+        alpha, beta = 2, 1
+        print('alpha = {}, beta = {}'.format(alpha, beta))
+        self.sig_Loc = Loc_Sigmoid(dim=2, alpha=alpha, beta=beta)
+        self.sig_glob = Glob_Sigmoid(dim=2, alpha=alpha, beta=beta, num_classes=num_classes)
+ 
+
+    def forward(self, x):
+        #x = self.dropout(F.relu(self.logits(F.relu(x))))
+        #x = self.dropout(F.relu(self.logits(x)))
+        x = self.dropout(F.relu(x))
+        cls_x = self.cls_logits(x)
+        loc_x = self.loc_logits(x)
+        if self._spatial_squeeze:
+            cls_x = cls_x.squeeze(3)
+            loc_x = loc_x.squeeze(3)
+        cls_x = self.s_Cls(cls_x)
+        loc_x = self.sig_Loc(loc_x)
+        return cls_x, loc_x
+
 
 class WSGN_sigmoid(nn.Module):
     def __init__(self, num_classes=400, spatial_squeeze=True, name='wsgn', dropout_keep_prob=0.8, mode='rgb'):
@@ -77,9 +120,9 @@ class WSGN_sigmoid(nn.Module):
         self.cls_logits = nn.Conv2d(in_channels=1024, out_channels=self._num_classes, kernel_size=1)
         self.loc_logits = nn.Conv2d(in_channels=1024, out_channels=self._num_classes, kernel_size=1)
         self.s_Cls = nn.Softmax(1)
-        alpha, beta = 2, 1.5
+        alpha, beta = 1, 1
         print('alpha = {}, beta = {}'.format(alpha, beta))
-        self.sig_Loc = Loc_Sigmoid(dim=1, alpha=alpha, beta=beta)
+        self.sig_Loc = Loc_Sigmoid(dim=2, alpha=alpha, beta=beta)
  
 
     def forward(self, x):
